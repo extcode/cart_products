5b6a633730bff1ed5ca293e6a3dd62eb88a73c1d
Daniel Lorenz <daniel.lorenz@extco.de>
2018-08-27 19:20:57 +0200

    [TASK] #10 - extract reaser plugin

--- ext_tables.php
+++ ext_tables.php
@@ -16,12 +16,21 @@ $_LLL = 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf';
  * Register Frontend Plugins
  */
 $pluginNames = [
-    'Products',
-    'SingleProduct',
-    'ProductPartial',
+    'Products' => [
+        'subtypes_excludelist' => 'select_key'
+    ],
+    'TeaserProducts' => [
+        'subtypes_excludelist' => 'select_key, pages, recursive'
+    ],
+    'SingleProduct' => [
+        'subtypes_excludelist' => 'select_key, pages, recursive'
+    ],
+    'ProductPartial' => [
+        'subtypes_excludelist' => 'select_key, recursive'
+    ],
 ];
 
-foreach ($pluginNames as $pluginName) {
+foreach ($pluginNames as $pluginName => $pluginConf) {
     $pluginSignature = strtolower(str_replace('_', '', $_EXTKEY)) . '_' . strtolower($pluginName);
     $pluginNameSC = strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst($pluginName)));
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
@@ -30,11 +39,7 @@ foreach ($pluginNames as $pluginName) {
         $_LLL . ':tx_cartproducts.plugin.' . $pluginNameSC . '.title'
     );
 
-    if ($pluginName == 'SingleProduct') {
-        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key, pages, recursive';
-    } else {
-        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
-    }
+    $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = $pluginConf['subtypes_excludelist'];
 
     $flexFormPath = 'EXT:' . $_EXTKEY . '/Configuration/FlexForms/' . $pluginName . 'Plugin.xml';
     if (file_exists(\TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($flexFormPath))) {
