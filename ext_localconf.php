eeb0a5fc869923763cb3de227521ebb14ceded4a
Daniel Lorenz <daniel.lorenz@extco.de>
2018-11-25 19:03:31 +0100

    [BUGFIX] use correct file extension for ContentElementWizard
    Fixes: #17

--- ext_localconf.php
+++ ext_localconf.php
@@ -77,7 +77,7 @@ if (TYPO3_MODE === 'BE') {
 // TSconfig
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:cart_products/Configuration/TSconfig/ContentElementWizard.typoscript">
+    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:cart_products/Configuration/TSconfig/ContentElementWizard.tsconfig">
 ');
 
 // Cart Hooks
