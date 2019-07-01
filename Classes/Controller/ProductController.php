a689036a87f131695b6f2f512b72be478399e8f3
Jonas Esser <j.esser@tmg-web.de>
2019-07-01 11:19:44 +0200

    [BUGFIX] Assign cartSettings in listAction to allow addToCart Button on listView

--- Classes/Controller/ProductController.php
+++ Classes/Controller/ProductController.php
@@ -179,6 +179,7 @@ class ProductController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
 
         $this->view->assign('searchArguments', $this->searchArguments);
         $this->view->assign('products', $products);
+        $this->view->assign('cartSettings', $this->cartSettings);
 
         $this->assignCurrencyTranslationData();
 
