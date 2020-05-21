11a1d8300b0acdc2b6d940fca2e157c5df12bca4
Jonathan Starck <info@cretection.eu>
2020-05-21 17:27:25 +0200

    [TASK] Make file compatible to TYPO3 v10.4

--- Classes/ViewHelpers/Backend/RemoveRecordViewHelper.php
+++ Classes/ViewHelpers/Backend/RemoveRecordViewHelper.php
@@ -12,7 +12,7 @@ use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
