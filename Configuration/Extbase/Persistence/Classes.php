33f926a25ec822e74c44060f21b28e30e5a27cc9
Thomas Off <t.off@datamints.com>
2020-09-17 12:38:14 +0200

    Make Extbase table mapping compatible with TYPO3 10.

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,22 +1,210 @@
 <?php
-declare(strict_types=1);
 
 return [
     \Extcode\CartProducts\Domain\Model\TtContent::class => [
         'tableName' => 'tt_content',
         'properties' => [
-            'altText' => [
-                'fieldName' => 'altText'
-            ],
-            'titleText' => [
-                'fieldName' => 'titleText'
-            ],
-            'colPos' => [
-                'fieldName' => 'colPos'
-            ],
-            'CType' => [
-                'fieldName' => 'CType'
-            ],
+            'crdate' => [
+				'fieldName' => 'crdate',
+			],
+			'tstamp' => [
+				'fieldName' => 'tstamp',
+			],
+			'contentType' => [
+				'fieldName' => 'CType',
+			],
+			'header' => [
+				'fieldName' => 'header',
+			],
+			'headerPosition' => [
+				'fieldName' => 'header_position',
+			],
+			'bodytext' => [
+				'fieldName' => 'bodytext',
+			],
+			'image' => [
+				'fieldName' => 'image',
+			],
+			'imagewidth' => [
+				'fieldName' => 'imagewidth',
+			],
+			'imageorient' => [
+				'fieldName' => 'imageorient',
+			],
+			'imagecaption' => [
+				'fieldName' => 'imagecaption',
+			],
+			'imagecols' => [
+				'fieldName' => 'imagecols',
+			],
+			'imageborder' => [
+				'fieldName' => 'imageborder',
+			],
+			'media' => [
+				'fieldName' => 'media',
+			],
+			'layout' => [
+				'fieldName' => 'layout',
+			],
+			'cols' => [
+				'fieldName' => 'cols',
+			],
+			'records' => [
+				'fieldName' => 'records',
+			],
+			'pages' => [
+				'fieldName' => 'pages',
+			],
+			'starttime' => [
+				'fieldName' => 'starttime',
+			],
+			'endtime' => [
+				'fieldName' => 'endtime',
+			],
+			'colPos' => [
+				'fieldName' => 'colPos',
+			],
+			'subheader' => [
+				'fieldName' => 'subheader',
+			],
+			'spaceBefore' => [
+				'fieldName' => 'spaceBefore',
+			],
+			'spaceAfter' => [
+				'fieldName' => 'spaceAfter',
+			],
+			'feGroup' => [
+				'fieldName' => 'fe_group',
+			],
+			'headerLink' => [
+				'fieldName' => 'header_link',
+			],
+			'imagecaptionPosition' => [
+				'fieldName' => 'imagecaption_position',
+			],
+			'imageLink' => [
+				'fieldName' => 'image_link',
+			],
+			'imageZoom' => [
+				'fieldName' => 'image_zoom',
+			],
+			'imageNoRows' => [
+				'fieldName' => 'image_noRows',
+			],
+			'imageEffects' => [
+				'fieldName' => 'image_effects',
+			],
+			'imageCompression' => [
+				'fieldName' => 'image_compression',
+			],
+			'altText' => [
+				'fieldName' => 'altText',
+			],
+			'titleText' => [
+				'fieldName' => 'titleText',
+			],
+			'longdescUrl' => [
+				'fieldName' => 'longdescURL',
+			],
+			'headerLayout' => [
+				'fieldName' => 'header_layout',
+			],
+			'menuType' => [
+				'fieldName' => 'menu_type',
+			],
+			'listType' => [
+				'fieldName' => 'list_type',
+			],
+			'tableBorder' => [
+				'fieldName' => 'table_border',
+			],
+			'tableCellspacing' => [
+				'fieldName' => 'table_cellspacing',
+			],
+			'tableCellpadding' => [
+				'fieldName' => 'table_cellpadding',
+			],
+			'tableBgColor' => [
+				'fieldName' => 'table_bgColor',
+			],
+			'selectKey' => [
+				'fieldName' => 'select_key',
+			],
+			'sectionIndex' => [
+				'fieldName' => 'sectionIndex',
+			],
+			'linkToTop' => [
+				'fieldName' => 'linkToTop',
+			],
+			'fileCollections' => [
+				'fieldName' => 'file_collections',
+			],
+			'filelinkSize' => [
+				'fieldName' => 'filelink_size',
+			],
+			'filelinkSorting' => [
+				'fieldName' => 'filelink_sorting',
+			],
+			'target' => [
+				'fieldName' => 'target',
+			],
+			'sectionFrame' => [
+				'fieldName' => 'section_frame',
+			],
+			'date' => [
+				'fieldName' => 'date',
+			],
+			'multimedia' => [
+				'fieldName' => 'multimedia',
+			],
+			'imageFrames' => [
+				'fieldName' => 'image_frames',
+			],
+			'recursive' => [
+				'fieldName' => 'recursive',
+			],
+			'imageheight' => [
+				'fieldName' => 'imageheight',
+			],
+			'rteEnabled' => [
+				'fieldName' => 'rte_enabled',
+			],
+			'sysLanguageUid' => [
+				'fieldName' => 'sys_language_uid',
+			],
+			'txImpexpOriguid' => [
+				'fieldName' => 'tx_impexp_origuid',
+			],
+			'piFlexform' => [
+				'fieldName' => 'pi_flexform',
+			],
+			'accessibilityTitle' => [
+				'fieldName' => 'accessibility_title',
+			],
+			'accessibilityBypass' => [
+				'fieldName' => 'accessibility_bypass',
+			],
+			'accessibilityBypassText' => [
+				'fieldName' => 'accessibility_bypass_text',
+			],
+			'selectedCategories' => [
+				'fieldName' => 'selected_categories',
+			],
+			'categoryField' => [
+				'fieldName' => 'category_field',
+			],
+			'txGridelementsBackendLayout' => [
+				'fieldName' => 'tx_gridelements_backend_layout',
+			],
+			'txGridelementsChildren' => [
+				'fieldName' => 'tx_gridelements_children',
+			],
+			'txGridelementsContainer' => [
+				'fieldName' => 'tx_gridelements_container',
+			],
+			'txGridelementsColumns' => [
+				'fieldName' => 'tx_gridelements_columns',
+			],
         ],
     ],
     \Extcode\CartProducts\Domain\Model\Category::class => [
