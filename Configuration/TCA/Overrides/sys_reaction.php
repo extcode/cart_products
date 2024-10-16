dfcf19dced02092d323e62f615a3be9ac694b584
Daniel Gohlke <daniel.gohlke@extco.de>
2024-10-16 23:38:10 +0200

    [BUGFIX] Make typo3/cms-reactions an optional dependency
    Relates: #233

--- Configuration/TCA/Overrides/sys_reaction.php
+++ Configuration/TCA/Overrides/sys_reaction.php
@@ -3,25 +3,27 @@
 use Extcode\CartProducts\Reaction\UpdateStockReaction;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-// Add the custom type to the type select
-ExtensionManagementUtility::addTcaSelectItem(
-    'sys_reaction',
-    'reaction_type',
-    [
-        'label' => UpdateStockReaction::getDescription(),
-        'value' => UpdateStockReaction::getType(),
-        'icon' => UpdateStockReaction::getIconIdentifier(),
-    ]
-);
+if (ExtensionManagementUtility::isLoaded('typo3/cms-reactions')) {
+    // Add the custom type to the type select
+    ExtensionManagementUtility::addTcaSelectItem(
+        'sys_reaction',
+        'reaction_type',
+        [
+            'label' => UpdateStockReaction::getDescription(),
+            'value' => UpdateStockReaction::getType(),
+            'icon' => UpdateStockReaction::getIconIdentifier(),
+        ]
+    );
 
-// Type icon
-$GLOBALS['TCA']['sys_reaction']['ctrl']['typeicon_classes'][UpdateStockReaction::getType()] = UpdateStockReaction::getIconIdentifier();
+    // Type icon
+    $GLOBALS['TCA']['sys_reaction']['ctrl']['typeicon_classes'][UpdateStockReaction::getType()] = UpdateStockReaction::getIconIdentifier();
 
-// What fields to display
-$GLOBALS['TCA']['sys_reaction']['types'][UpdateStockReaction::getType()] = [
-    'showitem' => '
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-        --palette--;;config,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-        --palette--;;access',
-];
+    // What fields to display
+    $GLOBALS['TCA']['sys_reaction']['types'][UpdateStockReaction::getType()] = [
+        'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;config,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;access',
+    ];
+}
