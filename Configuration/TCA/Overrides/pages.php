2abac9a86476b064d330f6a0fd36138fac5ff567
Daniel Gohlke <daniel.gohlke@extco.de>
2026-04-14 22:14:32 +0200

    [BUGFIX] Add type configuration for page wizard

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,30 +2,60 @@
 
 defined('TYPO3') or die();
 
+use Extcode\CartProducts\Constants;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(function () {
     $_LLL_be = 'LLL:' . 'EXT:cart_products/Resources/Private/Language/locallang_be.xlf:';
 
-    $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-        'label' => $_LLL_be . 'pages.doktype.182',
-        'value' => 182,
-        'icon' => 'apps-pagetree-page-cartproducts-products',
-    ];
-    $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-        'label' => $_LLL_be . 'pages.doktype.183',
-        'value' => 183,
-        'icon' => 'apps-pagetree-page-cartproducts-products',
-    ];
-    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-        'label' => 'LLL:EXT:cart_products/Resources/Private/Language/locallang_be.xlf:tcarecords-pages-contains.cart_products',
-        'value' => 'cartproducts',
-        'icon' => 'apps-pagetree-folder-cartproducts-products',
-    ];
-
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][182] = 'apps-pagetree-page-cartproducts-products';
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][183] = 'apps-pagetree-page-cartproducts-products';
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-cartproducts'] = 'apps-pagetree-folder-cartproducts-products';
+    ArrayUtility::mergeRecursiveWithOverrule(
+        $GLOBALS['TCA']['pages'],
+        [
+            'columns' => [
+                'doktype' => [
+                    'config' => [
+                        'items' => [
+                            1776196019 => [
+                                'label' => $_LLL_be . 'pages.doktype.' . Constants::DOKTYPE_CARTPRODUCTS_PRODUCTS,
+                                'value' => Constants::DOKTYPE_CARTPRODUCTS_PRODUCTS,
+                                'icon' => 'apps-pagetree-page-cartproducts-products',
+                                'group' => 'default',
+                            ],
+                            1776196036 => [
+                                'label' => $_LLL_be . 'pages.doktype.' . Constants::DOKTYPE_CARTPRODUCTS_PRODUCT,
+                                'value' => Constants::DOKTYPE_CARTPRODUCTS_PRODUCT,
+                                'icon' => 'apps-pagetree-page-cartproducts-products',
+                                'group' => 'default',
+                            ],
+                        ],
+                    ],
+                ],
+                'module' => [
+                    'config' => [
+                        'items' => [
+                            1776196062 => [
+                                'label' => $_LLL_be . 'tcarecords-pages-contains.cart_products',
+                                'value' => 'cartproducts',
+                                'icon' => 'apps-pagetree-folder-cartproducts-products',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'ctrl' => [
+                'typeicon_classes' => [
+                    Constants::DOKTYPE_CARTPRODUCTS_PRODUCTS => 'apps-pagetree-page-cartproducts-products',
+                    Constants::DOKTYPE_CARTPRODUCTS_PRODUCT => 'apps-pagetree-page-cartproducts-products',
+                    'contains-cartproducts' => 'apps-pagetree-folder-cartproducts-products',
+                ],
+            ],
+            'types' => [
+                Constants::DOKTYPE_CARTPRODUCTS_PRODUCTS => $GLOBALS['TCA']['pages']['types'][(string)\TYPO3\CMS\Core\Domain\Repository\PageRepository::DOKTYPE_DEFAULT],
+                Constants::DOKTYPE_CARTPRODUCTS_PRODUCT => $GLOBALS['TCA']['pages']['types'][(string)\TYPO3\CMS\Core\Domain\Repository\PageRepository::DOKTYPE_DEFAULT],
+            ],
+        ]
+    );
 
     $newPagesColumns = [
         'cart_products_product' => [
